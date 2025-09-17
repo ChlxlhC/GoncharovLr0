8abc6715416ec8358bfe2f33ee2cf91c42c5e410
ChlxlhC <pashgonch@gmail.com>
2025-09-17 10:24:58 +0300

    Lr0

---
+++ Lr0/Lr0.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream> 
+using namespace std; 
+
+int main()           
+{
+    cout << "Hello world" << endl;
+
+    char symbol;
+
+    cout << "Введите любой символ: ";
+    cin >> symbol;
+
+    cout << "Вы ввели: '" << symbol << "'" << endl;
+
+    return 0;       
+}
